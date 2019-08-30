0db1c46c5aa0261af9cb8a54ce9e2034b74c7219
Bernice Carbaugh <bernicecarbaugh@yahoo.com>
2019-08-29 17:27:27 -0700

    home page map modal with some content

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -1,36 +1,106 @@
 import React, { Component } from "react";
-import { Jumbotron } from "reactstrap";
-
-function RenderMapArea({ property }) {
-  return (
-    <area
-      href="#"
-      // onClick={this.handleMapArea}
-      shape="circle"
-      coords={`${property.mapCoord},12`}
-    />
-  );
-}
+import {
+  Jumbotron,
+  Modal,
+  ModalHeader,
+  ModalBody,
+  Button,
+  Row
+} from "reactstrap";
+import Carousel from "react-bootstrap/Carousel";
 
 class Home extends Component {
   constructor(props) {
     super(props);
-    this.handleMapArea = this.handleMapArea.bind(this);
+    this.state = {
+      selectedPropId: 0,
+      isModalOpen: false
+    };
+    this.handleMapClick = this.handleMapClick.bind(this);
+    this.closeModal = this.closeModal.bind(this);
   }
 
-  handleMapArea() {
-    console.log("hello handle map area invooked");
-  }
+  handleMapClick = props => e => {
+    this.setState({
+      selectedPropId: props.property.propId,
+      isModalOpen: true
+    });
+  };
 
-  //   const renderModal = property => {
-  //   console.log(property);
-  //   return null;
-  // };
+  closeModal() {
+    this.setState({
+      selectedPropId: 0,
+      isModalOpen: false
+    });
+  }
 
   render() {
     const mapareas = this.props.properties.map(property => {
-      return <RenderMapArea property={property} />;
+      return (
+        <area
+          id={property.propId}
+          onClick={this.handleMapClick({ property })}
+          href="#"
+          shape="circle"
+          coords={property.mapCoord}
+        />
+      );
     });
+
+    const ModalContent = ({ property }) => {
+      if (property !== undefined) {
+        return (
+          <React.Fragment>
+            <ModalHeader>
+              {property.title}
+              <div className="small">{property.neighborhood}</div>
+            </ModalHeader>
+            <ModalBody>
+              <div className="row">
+                <div className="col-12 col-sm-6">
+                  <Carousel>
+                    <Carousel.Item>
+                      <img
+                        className="d-block w-100 img-modal-carousel"
+                        src={property.imgB4Url}
+                        alt="Before"
+                      />
+                      <Carousel.Caption>
+                        <p>Before</p>
+                      </Carousel.Caption>
+                    </Carousel.Item>
+                    <Carousel.Item>
+                      <img
+                        className="d-block w-100 img-modal-carousel"
+                        src={property.imgPostUrl}
+                        alt="After"
+                      />
+                      <Carousel.Caption>
+                        <p>After</p>
+                      </Carousel.Caption>
+                    </Carousel.Item>
+                  </Carousel>
+                </div>
+
+                <div className="col-12 col-sm-6 align-self-center">
+                  {property.description}
+                  <hr />
+                  <a className="btn btn-primary mt-2 mr-2"> Read More</a>
+                  <Button
+                    className="btn btn-seconary mt-2"
+                    onClick={this.closeModal}
+                  >
+                    Close
+                  </Button>
+                </div>
+              </div>
+            </ModalBody>
+          </React.Fragment>
+        );
+      } else {
+        return <ModalBody>nothing</ModalBody>;
+      }
+    };
 
     return (
       <React.Fragment>
@@ -57,6 +127,16 @@ class Home extends Component {
         </Jumbotron>
 
         <div className="container">
+          <Modal isOpen={this.state.isModalOpen}>
+            <ModalContent
+              property={
+                this.props.properties.filter(
+                  property => property.propId === this.state.selectedPropId
+                )[0]
+              }
+            />
+          </Modal>
+
           <img
             className="map"
             src="/images/seattle-map.png"
